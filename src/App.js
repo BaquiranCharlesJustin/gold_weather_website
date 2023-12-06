03ba0adfc1830eff5ecaf1d2c58b83fb520764a8
CyjKun <lightningpatapon@gmail.com>
2023-12-07 01:55:33 +0800

    Revamp react app with tailwindcss

--- src/App.js
+++ src/App.js
@@ -1,18 +1,11 @@
-import Welcome from './pages/Welcome';
-import Home from './pages/Home';
+import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <Router>
-        <Routes>
-          <Route exact path="/" element={<Welcome />} />
-          <Route path="/Home" element={<Home />} />
-        </Routes>
-      </Router>
-    </div>
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
   );
 }
 
